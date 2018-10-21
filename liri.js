e340b249bf1f20374b55a96483ce135a196b8f45
Doctor Mew <trc379@gmail.com>
2018-10-20 18:02:18 -0700

    Added 3 of the 4 Things

--- liri.js
+++ liri.js
@@ -3,58 +3,115 @@ require("dotenv").config()
 
 var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
 
-var spotify = new spotify(keys.spotify);
+
+var spotify = new Spotify(keys.spotify);
+
 
 var search = "";
 
-for(var i = 3; i < process.argv.length; i++)
-{
-    search += process.argv[i] + " ";
-}
 
 var command = process.argv[2];
 
-if(command === "concert-this")
-{
-
-}
-else if (command === "spotify-this-song")
-{
-
-}
-else if (command === "movie-this")
-{
+function MovieThis(search) {
+    for (var i = 3; i < process.argv.length; i++) {
+        search += process.argv[i] + " ";
+    }
     url = "http://www.omdbapi.com/?t=" + search + "&apikey=trilogy"
-    request(url , function(error, response , body) {
+    request(url, function (error, response, body) {
 
-        if(error)
-        {
+        if (error) {
             console.log(error);
         }
-
         var jsonData = JSON.parse(body);
 
         var movieData = [
             "Movie: " + jsonData.Title,
             "Year: " + jsonData.Year,
-            "IMDB Rating: " + jsonData.Ratings[0].value,
-            "Rotten Tomatoes: " + jsonData.Ratings[1].value,
+            "IMDB Rating: " + jsonData.Ratings[0].Value,
+            "Rotten Tomatoes: " + jsonData.Ratings[1].Value,
             "Country: " + jsonData.Country,
             "Language : " + jsonData.Language,
             "Plot: " + jsonData.Plot,
             "Actors : " + jsonData.Actors
 
-          ].join("\n\n");
+        ].join("\n\n");
 
-          console.log(movieData);
-    })
-}
-else if (command === "do-what-it-says")
-{
+        console.log(movieData);
+    });
 
 }
-else
+
+function ConcertThis(band)
 {
-    console.log("Please enter a valid command");
+console.log(band);
+ request("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp" , function(error , data) {
+
+ if(error)
+ {
+     console.log(error);
+ }
+
+ console.log(data);
+ });
 }
+
+function SpotifyThis(song)
+{
+    spotify.search({ type : "track" , query : song}, function(error,data)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+     var songD = data.tracks.items[0];
+     var songData = [
+             "Artist(s): " + songD.album.artists[0].name,
+             "Name: " + songD.name,
+             "Spotify Link: " + songD.album.external_urls.spotify,
+             "Album: " + songD.album.name
+         ].join("\n\n");
+    console.log(songData);
+
+})
+
+}
+
+
+function CommandLine(command)
+{
+    for (var i = 3; i < process.argv.length; i++) {
+        search += process.argv[i] + " ";
+    }
+    if (command === "concert-this") {
+        ConcertThis(search);
+    }
+    else if (command === "spotify-this-song") {
+        SpotifyThis(search);
+    }
+    else if (command === "movie-this") {
+        MovieThis(search);
+    }
+    else if (command === "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+
+            if (error) {
+                console.log(error);
+            }
+    
+            console.log(data);
+            var data2 = data.split(",");
+            command = data2[0];
+            search = data2[1];
+            CommandLine(command);
+    
+        })
+    }
+    else {
+        console.log("Please enter a valid command");
+    }
+}
+
+CommandLine(command);
